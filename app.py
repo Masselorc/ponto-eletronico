fbb450d9498adf045df09c16c1b9674f81e60c0d
Masselorc <cortmr@gmail.com>
2025-04-23 10:58:05 -0300

    Versão 0.2
    1) Modifiquei os arquivos wsgi.py e app.py para usar a variável de ambiente PORT do Render com valor padrão 10000.
    
    2) Substituí a referência dinâmica {{ now.year }} no template base.html por um valor estático "2025".
    
    3) Adicionei código no wsgi.py para garantir que as variáveis de ambiente necessárias (SECRET_KEY, DATABASE_URI, DEBUG) estejam sempre disponíveis.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import os
 from app import create_app
 
 application = create_app()
 app = application
 
 if __name__ == '__main__':
-    application.run()
+    port = int(os.environ.get('PORT', 10000))
+    application.run(host='0.0.0.0', port=port)
