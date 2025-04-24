7da9d39628e34d8d12982cada54f2932f84ae156
Masselorc <cortmr@gmail.com>
2025-04-24 16:10:19 -0300

    v79

--- app/forms/ponto.py
+++ app/forms/ponto.py
@@ -4,19 +4,26 @@ from wtforms.validators import DataRequired, Optional, Length
 from datetime import date
 
 class RegistroPontoForm(FlaskForm):
-    """Formulário para registro de ponto."""
-    data = DateField('Data', validators=[DataRequired()], format='%Y-%m-%d')
-    # CORREÇÃO DEFINITIVA: Campo oculto para armazenar a data original como string
+    """Formulário para registro de ponto simples (apenas entrada)"""
+    # CORREÇÃO DEFINITIVA: Adicionado campo oculto para armazenar a data original como string
     data_original = HiddenField('Data Original')
-    entrada = TimeField('Entrada', format='%H:%M', validators=[DataRequired()])
+    
+    # CORREÇÃO DEFINITIVA: Removido default=date.today para forçar o usuário a selecionar uma data
+    data = DateField('Data', validators=[DataRequired()])
     observacoes = TextAreaField('Observações', validators=[Optional(), Length(max=500)])
 
 class RegistroMultiploPontoForm(FlaskForm):
-    """Formulário para registro múltiplo de ponto."""
-    data = DateField('Data', validators=[DataRequired()], format='%Y-%m-%d')
-    # CORREÇÃO DEFINITIVA: Campo oculto para armazenar a data original como string
+    """Formulário para registro de ponto completo (entrada, saída, almoço, etc.)"""
+    # CORREÇÃO DEFINITIVA: Adicionado campo oculto para armazenar a data original como string
     data_original = HiddenField('Data Original')
-    afastamento = BooleanField('Afastamento')
+    
+    # CORREÇÃO DEFINITIVA: Removido default=date.today para forçar o usuário a selecionar uma data
+    data = DateField('Data', validators=[DataRequired()])
+    entrada = StringField('Entrada (HH:MM)', validators=[Optional()])
+    saida_almoco = StringField('Saída para Almoço (HH:MM)', validators=[Optional()])
+    retorno_almoco = StringField('Retorno do Almoço (HH:MM)', validators=[Optional()])
+    saida = StringField('Saída (HH:MM)', validators=[Optional()])
+    afastamento = BooleanField('Afastamento', default=False)
     tipo_afastamento = SelectField('Tipo de Afastamento', 
                                   choices=[
                                       ('', 'Selecione'),
@@ -27,19 +34,21 @@ class RegistroMultiploPontoForm(FlaskForm):
                                       ('outro', 'Outro')
                                   ],
                                   validators=[Optional()])
-    entrada = StringField('Entrada (HH:MM)', validators=[Optional()])
-    saida_almoco = StringField('Saída para Almoço (HH:MM)', validators=[Optional()])
-    retorno_almoco = StringField('Retorno do Almoço (HH:MM)', validators=[Optional()])
-    saida = StringField('Saída (HH:MM)', validators=[Optional()])
     observacoes = TextAreaField('Observações', validators=[Optional(), Length(max=500)])
     atividades = TextAreaField('Atividades Realizadas', validators=[Optional(), Length(max=1000)])
 
 class EditarPontoForm(FlaskForm):
-    """Formulário para edição de ponto."""
-    data = DateField('Data', validators=[DataRequired()], format='%Y-%m-%d')
-    # CORREÇÃO DEFINITIVA: Campo oculto para armazenar a data original como string
+    """Formulário para edição de ponto"""
+    # CORREÇÃO DEFINITIVA: Adicionado campo oculto para armazenar a data original como string
     data_original = HiddenField('Data Original')
-    afastamento = BooleanField('Afastamento')
+    
+    # CORREÇÃO DEFINITIVA: Removido default=date.today para forçar o usuário a selecionar uma data
+    data = DateField('Data', validators=[DataRequired()])
+    entrada = StringField('Entrada (HH:MM)', validators=[Optional()])
+    saida_almoco = StringField('Saída para Almoço (HH:MM)', validators=[Optional()])
+    retorno_almoco = StringField('Retorno do Almoço (HH:MM)', validators=[Optional()])
+    saida = StringField('Saída (HH:MM)', validators=[Optional()])
+    afastamento = BooleanField('Afastamento', default=False)
     tipo_afastamento = SelectField('Tipo de Afastamento', 
                                   choices=[
                                       ('', 'Selecione'),
@@ -50,12 +59,8 @@ class EditarPontoForm(FlaskForm):
                                       ('outro', 'Outro')
                                   ],
                                   validators=[Optional()])
-    entrada = StringField('Entrada (HH:MM)', validators=[Optional()])
-    saida_almoco = StringField('Saída para Almoço (HH:MM)', validators=[Optional()])
-    retorno_almoco = StringField('Retorno do Almoço (HH:MM)', validators=[Optional()])
-    saida = StringField('Saída (HH:MM)', validators=[Optional()])
     observacoes = TextAreaField('Observações', validators=[Optional(), Length(max=500)])
 
 class AtividadeForm(FlaskForm):
-    """Formulário para registro de atividade."""
+    """Formulário para registro de atividade"""
     descricao = TextAreaField('Descrição da Atividade', validators=[DataRequired(), Length(max=1000)])
