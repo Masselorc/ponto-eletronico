d0a058cb7e012926e251ab7586e5d5aa9c6ca00c
Masselorc <cortmr@gmail.com>
2025-04-24 08:11:53 -0300

    v39

--- app/models/ponto.py
+++ app/models/ponto.py
@@ -1,5 +1,5 @@
 from app import db
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 
 class Ponto(db.Model):
     __tablename__ = 'pontos'
@@ -7,16 +7,42 @@ class Ponto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     data = db.Column(db.Date, nullable=False, default=date.today)
-    entrada = db.Column(db.Time, nullable=True)
-    saida_almoco = db.Column(db.Time, nullable=True)
-    retorno_almoco = db.Column(db.Time, nullable=True)
-    saida = db.Column(db.Time, nullable=True)
+    entrada = db.Column(db.DateTime, nullable=True)
+    saida_almoco = db.Column(db.DateTime, nullable=True)
+    retorno_almoco = db.Column(db.DateTime, nullable=True)
+    saida = db.Column(db.DateTime, nullable=True)
     horas_trabalhadas = db.Column(db.Float, nullable=True)
     afastamento = db.Column(db.Boolean, default=False)
     tipo_afastamento = db.Column(db.String(100), nullable=True)
+    observacoes = db.Column(db.Text, nullable=True)
     
     # Relacionamentos
     atividades = db.relationship('Atividade', backref='ponto', lazy=True, cascade="all, delete-orphan")
+    
+    def calcular_horas_trabalhadas(self):
+        """Calcula as horas trabalhadas com base nos horários registrados"""
+        if self.afastamento:
+            self.horas_trabalhadas = 0
+            return
+            
+        total_horas = 0
+        
+        # Período da manhã (entrada até saída para almoço)
+        if self.entrada and self.saida_almoco:
+            delta_manha = self.saida_almoco - self.entrada
+            total_horas += delta_manha.total_seconds() / 3600
+            
+        # Período da tarde (retorno do almoço até saída)
+        if self.retorno_almoco and self.saida:
+            delta_tarde = self.saida - self.retorno_almoco
+            total_horas += delta_tarde.total_seconds() / 3600
+            
+        # Se não tiver registro de almoço, mas tiver entrada e saída
+        if self.entrada and self.saida and not (self.saida_almoco or self.retorno_almoco):
+            delta_total = self.saida - self.entrada
+            total_horas = delta_total.total_seconds() / 3600
+            
+        self.horas_trabalhadas = round(total_horas, 1)
     
     def __repr__(self):
         return f'<Ponto {self.data} - Usuário {self.user_id}>'
@@ -34,10 +60,27 @@ class Atividade(db.Model):
 
 class Feriado(db.Model):
     __tablename__ = 'feriados'
+    __table_args__ = {'extend_existing': True}
     
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.Date, nullable=False, unique=True)
     descricao = db.Column(db.String(100), nullable=False)
+    
+    @property
+    def nome(self):
+        return self.descricao
+        
+    @property
+    def tipo(self):
+        return 'nacional'
+        
+    @property
+    def created_at(self):
+        return datetime.now()
+        
+    @property
+    def updated_at(self):
+        return datetime.now()
     
     def __repr__(self):
         return f'<Feriado {self.data} - {self.descricao}>'
